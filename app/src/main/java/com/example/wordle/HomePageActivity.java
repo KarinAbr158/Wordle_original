03df2b1b572be789ea5efd49a830ddc284d76f62
Karin <abramowitzkarin@gmail.com>
2025-11-30 16:04:11 +0200

    ughhhhhh

--- app/src/main/java/com/example/wordle/HomePageActivity.java
+++ app/src/main/java/com/example/wordle/HomePageActivity.java
@@ -1,6 +1,9 @@
 package com.example.wordle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomePageActivity extends AppCompatActivity {
 
+    Button startGame, settings, exit;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,33 @@ public class HomePageActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        startGame = findViewById(R.id.startBtn);
+        settings = findViewById(R.id.settingsBtn);
+        exit = findViewById(R.id.exitBtn);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(HomePageActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(HomePageActivity.this, GameActivity.class);
+                startActivity(i);
+            }
+        });
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
         });
     }
 }
