1b40562d42f4c797b33e5a6058df267351527689
Karin <abramowitzkarin@gmail.com>
2025-12-20 22:45:28 +0200

    נמאס לי אני מתפטרת

--- app/src/main/java/com/example/wordle/GameActivity.java
+++ app/src/main/java/com/example/wordle/GameActivity.java
@@ -1,5 +1,6 @@
 package com.example.wordle;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +25,7 @@ public class GameActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
         Log.v("GameActivity", "started onCreate");
+
         keyboard = findViewById(R.id.keyboardlayout);
         createWordleKeyboard();
     }
@@ -37,9 +40,33 @@ public class GameActivity extends AppCompatActivity {
         addKeyboardRow(row3, 2);
     }
 
-    private void addKeyboardRow(String[] letters, int rowIndex) {
+    private void addKeyboardRow(String[] keys, int row) {
+        int col = 0;
 
-        for (int i = 0; i < letters.length; i++) {
+        for (String text : keys) {
+            Button b = new Button(this);
+            b.setText(text);
+            b.setAllCaps(true);
+            b.setTextSize(14);
+
+            int span = (text.equals("ENTER") || text.equals("⌫")) ? 2 : 1;
+
+            GridLayout.LayoutParams p = new GridLayout.LayoutParams();
+            p.rowSpec = GridLayout.spec(row);
+            p.columnSpec = GridLayout.spec(col, span, 1f);
+            p.width = 0;
+            p.setMargins(4, 4, 4, 4);
+
+            b.setLayoutParams(p);
+
+            b.setOnClickListener(v ->
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+            );
+
+            keyboard.addView(b);
+            col += span;
+
+        /*for (int i = 0; i < letters.length; i++) {
             Button key = new Button(this);
 
             key.setText(letters[i]);
@@ -51,7 +78,7 @@ public class GameActivity extends AppCompatActivity {
             params.columnSpec = GridLayout.spec(i);
             params.setMargins(4, 4, 4, 4); // Adjust if needed
             key.setLayoutParams(params);
-            keyboard.addView(key);
+            keyboard.addView(key);*/
         }
     }
 }
