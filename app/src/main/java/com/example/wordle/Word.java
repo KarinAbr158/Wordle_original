a38503b6504388a6e1a1e5e81093a18615b88d2f
Karin <abramowitzkarin@gmail.com>
2025-12-23 20:19:50 +0200

    kululululu 2.0

--- app/src/main/java/com/example/wordle/Word.java
+++ app/src/main/java/com/example/wordle/Word.java
@@ -1,4 +1,24 @@
 package com.example.wordle;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName="words")
 public class Word {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String word;
+
+    public int getID(){
+        return this.id;
+    }
+
+    public void setID(int id){
+        this.id = id;
+    }
+
+    public String getWord(){
+        return this.word;
+    }
 }
--- app/src/main/java/com/example/wordle/WordDatabase.java
+++ app/src/main/java/com/example/wordle/WordDatabase.java
@@ -1,4 +1,25 @@
 package com.example.wordle;
 
-public class WordDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordDatabase extends RoomDatabase {
+    private static WordDatabase instance;
+    public abstract WordDAO wordDao();
+
+    public static synchronized WordDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    WordDatabase.class,
+                    "word_database")
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instance;
+    }
 }
